dce3240dbf34ef7c47130092fc269769ef5e8c1c
AustroMelee <rohailleghari@gmail.com>
2025-06-16 18:24:22 -0400

    syntax fix

--- JS/location-battle-conditions.js
+++ JS/location-battle-conditions.js
@@ -1,31 +1,32 @@
 // FILE: location-battle-conditions.js
 'use strict';
 
-// This file now aggregates detailed environmental data from modular files.
-
-// Import individual location condition data from the same directory
-import { easternAirTempleConditions } from './data_location_eastern-air-temple.js';
-import { northernWaterTribeConditions } from './data_location_northern-water-tribe.js';
-import { baSingSeConditions } from './data_location_ba-sing-se.js';
-import { siWongDesertConditions } from './data_location_si-wong-desert.js';
-import { foggySwampConditions } from './data_location_foggy-swamp.js';
-import { boilingRockConditions } from './data_location_boiling-rock.js';
-import { fireNationCapitalConditions } from './data_location_fire-nation-capital.js';
-import { omashuConditions } from './data_location_omashu.js';
-import { greatDivideConditions } from './data_location_great-divide.js';
-import { kyoshiIslandConditions } from './data_location_kyoshi-island.js';
+// Import individual location condition objects
+import { baSingSe } from './data_location_ba-sing-se.js';
+import { boilingRock } from './data_location_boiling-rock.js';
+import { easternAirTemple } from './data_location_eastern-air-temple.js';
+import { fireNationCapital } from './data_location_fire-nation-capital.js';
+import { foggySwamp } from './data_location_foggy-swamp.js';
+import { greatDivide } from './data_location_great-divide.js';
+import { kyoshiIsland } from './data_location_kyoshi-island.js';
+import { northernWaterTribe } from './data_location_northern-water-tribe.js';
+import { omashu } from './data_location_omashu.js';
+import { siWongDesert } from './data_location_si-wong-desert.js';
 
 
-// This object will combine all the individual location data
+/**
+ * A map of location IDs to their battle conditions.
+ * This is the central export used by the battle engine to get location data.
+ */
 export const locationConditions = {
-    [easternAirTempleConditions.id]: easternAirTempleConditions,
-    [northernWaterTribeConditions.id]: northernWaterTribeConditions,
-    [baSingSeConditions.id]: baSingSeConditions,
-    [siWongDesertConditions.id]: siWongDesertConditions,
-    [foggySwampConditions.id]: foggySwampConditions,
-    [boilingRockConditions.id]: boilingRockConditions,
-    [fireNationCapitalConditions.id]: fireNationCapitalConditions,
-    [omashuConditions.id]: omashuConditions,
-    [greatDivideConditions.id]: greatDivideConditions,
-    [kyoshiIslandConditions.id]: kyoshiIslandConditions,
+    'ba-sing-se': baSingSe,
+    'boiling-rock': boilingRock,
+    'eastern-air-temple': easternAirTemple,
+    'fire-nation-capital': fireNationCapital,
+    'foggy-swamp': foggySwamp,
+    'great-divide': greatDivide,
+    'kyoshi-island': kyoshiIsland,
+    'northern-water-tribe': northernWaterTribe,
+    'omashu': omashu,
+    'si-wong-desert': siWongDesert
 };
