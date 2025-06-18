b0e4dac42c444984fcf4a8166f6146ef94439065
AustroMelee <rohailleghari@gmail.com>
2025-06-18 02:05:10 -0400

    your message

--- JS/location-battle-conditions.js
+++ JS/location-battle-conditions.js
@@ -9,3 +9,12 @@ import { allLocations } from './data_locations_index.js';
  * This is the central export used by the battle engine to get location data.
  */
 export const locationConditions = allLocations;
+
+/**
+ * Gets battle conditions for a specific location
+ * @param {string} locationId - The location ID
+ * @returns {Object|null} The location's battle conditions or null if not found
+ */
+export function getBattleConditionsForLocation(locationId) {
+    return allLocations[locationId] || null;
+}
